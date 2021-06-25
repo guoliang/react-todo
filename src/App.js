e86b74ce3b5bc5ade123de22db08602f0189a95e
Leung Wong <wguoliang1113@gmail.com>
2021-06-25 15:47:57 +0200

    Adds todo add form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 
 import Container from "react-bootstrap/Container";
 import ListGroup from "react-bootstrap/ListGroup";
+
+import TodoAddForm from "./components/TodoAddForm";
 import TodoItem from "./components/TodoItem";
 
 import "./App.css";
@@ -25,6 +27,8 @@ function App() {
 
 	return (
 		<Container>
+			<h1>Todo</h1>
+			<TodoAddForm allTodos={todos} addTodo={setTodo} />
 			<h2>Todo Items</h2>
 			<ListGroup>{todoList}</ListGroup>
 		</Container>
---
+++ src/components/TodoAddForm.js
@@ -0,0 +1,49 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+
+const TodoAddForm = ({ allTodos, addTodo }) => {
+	const InputFieldID = "todoTitleFieldId";
+	const TextareaID = "todoTextareaId";
+
+	const onSubmitForm = ($event) => {
+		$event.preventDefault();
+
+		const todoElements = $event.target.elements;
+
+		const newTodo = {
+			id: Math.random(),
+			title: todoElements.namedItem(InputFieldID).value,
+			todo: todoElements.namedItem(TextareaID).value,
+		};
+
+		addTodo([newTodo, ...allTodos]);
+	};
+
+	return (
+		<>
+			<h2>Add new</h2>
+			<Form onSubmit={onSubmitForm}>
+				<Form.Group controlId={InputFieldID}>
+					<Form.Label>Title</Form.Label>
+					<Form.Control type="text" />
+				</Form.Group>
+				<Form.Group controlId={TextareaID}>
+					<Form.Label>Todo</Form.Label>
+					<Form.Control as="textarea" rows={5} />
+				</Form.Group>
+				<Button variant="primary" type="submit">
+					Submit
+				</Button>
+			</Form>
+		</>
+	);
+};
+
+TodoAddForm.propTypes = {
+	allTodos: PropTypes.array.isRequired,
+	addTodo: PropTypes.func.isRequired,
+};
+export default TodoAddForm;
